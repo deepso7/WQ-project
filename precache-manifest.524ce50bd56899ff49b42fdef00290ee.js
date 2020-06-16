f8c4828b3e67626841079318cfc11294432ba16f
deeps <deeps.o7o8.y@gmail.com>
2020-06-16 19:21:52 +0530

    first commit

---
+++ precache-manifest.524ce50bd56899ff49b42fdef00290ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1968cb54ffce575da84d0e7cc2b334",
+    "url": "/index.html"
+  },
+  {
+    "revision": "437901cfc32941d039f4",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f1111d0c1157bd69ac34",
+    "url": "/static/css/main.1e1f4f10.chunk.css"
+  },
+  {
+    "revision": "437901cfc32941d039f4",
+    "url": "/static/js/2.87be73ea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.87be73ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1111d0c1157bd69ac34",
+    "url": "/static/js/main.4da9f63d.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
